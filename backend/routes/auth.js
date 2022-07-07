614291afd9b854f05fe85e167df4136f7c5dbdc0
darpan9460@gmail.com <darpan9460@gmail.com>
2022-07-07 15:52:06 +0530

    configuring auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,17 @@
 const express=require('express');
 const User=require("../models/User");
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
-router.get('/',(req,res)=>{
+router.post('/',[
+    body('name').isLength({min:3}),
+    body('email').isEmail(),
+    body('password').isLength({min:5})
+],(req,res)=>{
     console.log(req.body);
     const user= User(req.body);
     user.save();
     res.json(req.body);
 })
 
-module.exports = router
+module.exports = router;
